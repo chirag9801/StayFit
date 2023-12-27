6d817668d2c90661d3465b1e65f8f6486dbd676f
chirag9801 <chiragpatil78315@gmail.com>
2023-12-28 00:40:07 +0530

    completed

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -14,7 +14,7 @@ function ProtectedRoute(props) {
     try {
       dispatch(showLoading());
       const response = await axios.post(
-        "./api/user/get-user-info-by-id",
+        "https://stayfit-fr11.onrender.com/api/user/get-user-info-by-id",
         {
           token: localStorage.getItem("token"),
         },
